a1e05df0c56d05b9036d4e35ec10d6edb3d9613a
arielamzallag2003-beep <ariel.amzallag2003@gmail.com>
2026-01-30 13:04:28 +0100

    Exercice 3: FizzBuzz N-ieme

---
+++ src/exercices/exo_03.rs
@@ -0,0 +1,25 @@
+pub const NOM: &str = "Exercice 3: FizzBuzz N-ieme";
+
+pub fn run() -> String {
+    let a = 3;
+    let b = 5;
+    let c = 15;
+    let d = 17;
+    
+    format!(
+        "FizzBuzz({}) = {}\nFizzBuzz({}) = {}\nFizzBuzz({}) = {}\nFizzBuzz({}) = {}",
+        a, fizzbuzz(a),
+        b, fizzbuzz(b),
+        c, fizzbuzz(c),
+        d, fizzbuzz(d),
+    )
+}
+
+fn fizzbuzz(n: u32) -> String {
+    match (n % 3, n % 5) {
+        (0, 0) => "FizzBuzz".to_string(),
+        (0, _) => "Fizz".to_string(),
+        (_, 0) => "Buzz".to_string(),
+        _ => n.to_string(),
+    }
+}
--- src/exercices/mod.rs
+++ src/exercices/mod.rs
@@ -1,10 +1,12 @@
 pub mod exo_01;
 pub mod exo_02;
+pub mod exo_03;
 
 pub fn liste_exercices() -> Vec<(u8, String)> {
     vec![
         (1, exo_01::NOM.to_string()),
         (2, exo_02::NOM.to_string()),
+        (3, exo_03::NOM.to_string())
     ]
 }
 
@@ -12,6 +14,7 @@ pub fn get_nom(id: u8) -> String {
     match id {
         1 => exo_01::NOM.to_string(),
         2 => exo_02::NOM.to_string(),
+        3 => exo_03::NOM.to_string(),
         _ => "Inconnu".to_string(),
     }
 }
@@ -20,6 +23,7 @@ pub fn executer(id: u8) -> Result<String, String> {
     match id {
         1 => Ok(exo_01::run()),
         2 => Ok(exo_02::run()),
+        3 => Ok(exo_03::run()),
         _ => Err(format!("Exercice {} non trouve", id)),
     }
 }
